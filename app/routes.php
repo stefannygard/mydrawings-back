d523977f9f8cf86dce4d808a8f9d77339cfdc59b
stefannygard <stefan_nygard@hotmail.com>
2014-09-10 22:09:00 +0200

    dep

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://mydrawings.herokuapp.com');
+header('Access-Control-Allow-Origin: *');
 
 /*
 |--------------------------------------------------------------------------
