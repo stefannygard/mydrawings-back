f3d28512cfeca7ef42446d349def57399c63838a
stefannygard <stefan_nygard@hotmail.com>
2014-09-10 21:57:37 +0200

    dep

--- app/filters.php
+++ app/filters.php
@@ -13,7 +13,12 @@
 
 App::before(function($request)
 {
-	//
+    if (Request::getMethod() == "OPTIONS") {
+        $headers = array(
+            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'=> 'X-Requested-With, content-type',);
+        return Response::make('', 200, $headers);
+    }
 });
 
 
--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,5 @@
 <?php
+header('Access-Control-Allow-Origin: http://mydrawings.herokuapp.com');
 
 /*
 |--------------------------------------------------------------------------
